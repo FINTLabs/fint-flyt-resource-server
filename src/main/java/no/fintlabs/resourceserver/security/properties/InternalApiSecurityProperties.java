c6bfdedc73a46b1ffa04ff843b6fcd3a37b96eb4
Egil Ballestad <egil.ballestad@vigoiks.no>
2024-01-03 15:07:12 +0100

    revert last commit and change property name

--- src/main/java/no/fintlabs/resourceserver/security/properties/InternalApiSecurityProperties.java
+++ src/main/java/no/fintlabs/resourceserver/security/properties/InternalApiSecurityProperties.java
@@ -1,14 +1,10 @@
 package no.fintlabs.resourceserver.security.properties;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 
-import javax.annotation.PostConstruct;
-import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -17,21 +13,7 @@ import java.util.Map;
 @NoArgsConstructor
 public class InternalApiSecurityProperties extends ApiSecurityProperties {
 
-    @Value("${fint.flyt.resource-server.security.api.internal.authorized-orgid-role-pairs}")
-    private String authorizedOrgIdRolePairsJson;
-
-    private Map<String, List<String>> authorizedOrgIdRolePairs;
-
-    @PostConstruct
-    public void init() {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            this.authorizedOrgIdRolePairs = mapper.readValue(authorizedOrgIdRolePairsJson, new TypeReference<>() {
-            });
-        } catch (IOException e) {
-            throw new RuntimeException("Error parsing authorizedOrgIdRolePairs JSON", e);
-        }
-    }
+    private Map<String, List<String>> authorizedOrgIdRolePairs = Collections.emptyMap();
 
     @Override
     public String[] getPermittedAuthorities() {
