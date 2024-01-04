3c639366fa7cc123fdfb5d48b48420225a2eaa2d
Egil Ballestad <egil.ballestad@vigoiks.no>
2024-01-04 08:46:39 +0100

    parse json string as property to complex datatype

--- src/main/java/no/fintlabs/resourceserver/security/properties/InternalApiSecurityProperties.java
+++ src/main/java/no/fintlabs/resourceserver/security/properties/InternalApiSecurityProperties.java
@@ -1,9 +1,14 @@
 package no.fintlabs.resourceserver.security.properties;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -11,9 +16,24 @@ import java.util.Map;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Slf4j
 public class InternalApiSecurityProperties extends ApiSecurityProperties {
 
+    private String authorizedOrgIdRolePairsJson = "{}";
+
     private Map<String, List<String>> authorizedOrgIdRolePairs = Collections.emptyMap();
+
+    @PostConstruct
+    public void parseAndSetAuthorizedOrgIdRolePairs() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            this.authorizedOrgIdRolePairs = mapper.readValue(authorizedOrgIdRolePairsJson,
+                    new TypeReference<>() {
+                    });
+        } catch (IOException e) {
+            log.error("Error parsing authorizedOrgIdRolePairsJson: {}", e.getMessage(), e);
+        }
+    }
 
     @Override
     public String[] getPermittedAuthorities() {
