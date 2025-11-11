0dc0d2247f40ed1079e9258b9e02bbcda776ade5
Eivind Morch <eivind@morch.priv.no>
2025-11-11 13:56:01 +0100

    wip: Add new publishing scripts

---
+++ build.gradle.kts
@@ -0,0 +1,83 @@
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
+
+plugins {
+    id("org.springframework.boot") version "3.5.7" apply false
+    id("io.spring.dependency-management") version "1.1.7"
+    id("maven-publish")
+    id("java-library")
+    id("com.github.ben-manes.versions") version "0.53.0"
+}
+
+group = "no.novari"
+version = findProperty("version")?.toString() ?: "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+    withSourcesJar()
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.fintlabs.no/releases")
+    }
+    mavenCentral()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom(SpringBootPlugin.BOM_COORDINATES)
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("no.fintlabs:fint-kafka:5.0.0-rc-15")
+
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("no.fintlabs:fint-flyt-cache:1.2.5")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("cglib:cglib-nodep:3.3.0")
+    testImplementation("org.springframework.security:spring-security-test")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://repo.fintlabs.no/releases")
+            credentials {
+                username = System.getenv("REPOSILITE_USERNAME")
+                password = System.getenv("REPOSILITE_PASSWORD")
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
