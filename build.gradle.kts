e2242282d731f0232590e51ede677ae9b48c48bc
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-08-21 08:01:55 +0200

    fix: bump lz4-java til 1.11.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ repositories {
 
 dependencies {
     constraints {
-        implementation("at.yawk.lz4:lz4-java:1.11.1") {
+        implementation("at.yawk.lz4:lz4-java:1.11.2") {
             because("Fixes CVE-2026-59949 in the kafka-clients transitive dependency")
         }
     }
